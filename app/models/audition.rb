499425bb8067603e378e0247d50cd514d08a7cf2
layersony <layersony@gmail.com>
2022-08-16 13:42:03 +0300

    Complete Assignment

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -1,3 +1,33 @@
 class Audition < ActiveRecord::Base
-  
-end
+    belongs_to :role
+
+
+    # Audition#call_back will change the the hired attribute to true
+    #### #<Audition>.call_back
+    def call_back
+
+
+        puts "- in call_back Method -"
+
+        #### using .save
+
+            # # self == #<Audition>
+            # self.hired = true 
+
+            # self.save
+            # # to PERSIST change to db
+
+        #### using .save
+
+
+        #### using .update
+
+            self.update( hired: true )
+
+        #### using .update
+
+        
+    end
+
+    
+end  #### THE LAST  end
